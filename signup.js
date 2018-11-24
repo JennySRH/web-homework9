170db7a919f8a1ed7729bf969faada3ffefcaa7b
Jenny <1056627011@qq.com>
2018-11-24 13:12:13 +0800

    first edition

---
+++ signup.js
@@ -0,0 +1,28 @@
+$(function(){
+    $('input:not(.button)').blur(function(){
+        if(validator.isFieldValid(this.id,$(this).val())) {
+            $(this).parent().find('.error').text('').hide();
+        } else {
+            $(this).parent().find('.error').text(validator.form[this.id].errorMessage).show();    
+
+        }
+    });
+
+    $('input.button.submit').click(function() {
+        $('input:not(.button)').blur();
+        if(!validator.isFormValid()) {
+            console.log(validator.form.username.status);
+            console.log(validator.form.sid.status);
+            console.log(validator.form.phone.status);
+            console.log(validator.form.email.status);
+            return false;
+        }
+        //return true;
+    });
+
+    $('input.button.reset').click(function() {
+        console.log("hi");
+        return true;
+    });
+
+});
